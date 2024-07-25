17fbf1383b1109190e5665385a49997b6f286fc5
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-25 17:32:12 +0500

    FOUNDATIONS: Add Applicants Service

---
+++ ManageMe.Core.Api/Services/Foundations/Applicants/IApplicantService.cs
@@ -0,0 +1,14 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Models.Applicants;
+
+namespace ManageMe.Core.Api.Services.Foundations.Applicants
+{
+    public interface IApplicantService
+    {
+        ValueTask<Applicant> AddApplicantAsync(Applicant applicant);
+    }
+}
