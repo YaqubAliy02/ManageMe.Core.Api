8d01cfec4a6863a6e32697416137f90d61f51e77
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-25 18:24:18 +0500

    ShouldAddApplicantAsync -> FAIL

---
+++ ManageMe.Core.Api.Tests.Unit/Services/Foundations/ApplicantServiceTests.cs
@@ -0,0 +1,60 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using System.Linq.Expressions;
+using ManageMe.Core.Api.Brokers.DateTimes;
+using ManageMe.Core.Api.Brokers.Loggings;
+using ManageMe.Core.Api.Brokers.Storages;
+using ManageMe.Core.Api.Models.Applicants;
+using ManageMe.Core.Api.Services.Foundations.Applicants;
+using Moq;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace ManageMe.Core.Api.Tests.Unit.Services.Foundations
+{
+    public partial class ApplicantServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+
+        private readonly IApplicantService applicantService;
+
+        public ApplicantServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.applicantService = new ApplicantService(
+               storageBroker: this.storageBrokerMock.Object,
+               loggingBroker: this.loggingBrokerMock.Object,
+               dateTimeBroker: this.dateTimeBrokerMock.Object
+                );
+        }
+
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private Applicant CreateRandomApplicant() =>
+            CreateApplicantFiller(GetRandomDateTime()).Create();
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<Applicant> CreateApplicantFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Applicant>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+
+        
+    }
+}
--- ManageMe.Core.Api/Services/Foundations/Applicants/ApplicantService.cs
+++ ManageMe.Core.Api/Services/Foundations/Applicants/ApplicantService.cs
@@ -3,6 +3,7 @@
 // Manage your academy easily
 //===========================
 
+using ManageMe.Core.Api.Brokers.DateTimes;
 using ManageMe.Core.Api.Brokers.Loggings;
 using ManageMe.Core.Api.Brokers.Storages;
 using ManageMe.Core.Api.Models.Applicants;
@@ -13,11 +14,16 @@ namespace ManageMe.Core.Api.Services.Foundations.Applicants
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
 
-        public ApplicantService(IStorageBroker storageBroker, ILoggingBroker loggingBroker)
+        public ApplicantService(
+            IStorageBroker storageBroker, 
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
         {
             this.storageBroker = storageBroker;
             this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
         }
 
         public ValueTask<Applicant> AddApplicantAsync(Applicant applicant)
