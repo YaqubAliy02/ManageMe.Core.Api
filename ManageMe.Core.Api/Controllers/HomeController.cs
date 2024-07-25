bcb7a209b1f508015558a3a51fdfae9a2d3fbabd
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-25 16:46:20 +0500

    DATA: Add HomeController

---
+++ ManageMe.Core.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageMe.Core.Api.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Welcome to MasterStream Project");
+        }
+    }
+}
