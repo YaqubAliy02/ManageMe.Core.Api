5a9ea6f4cdc3d0a5403cad1213531554336ee70d
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-25 08:28:26 +0500

    DATA: Add All Models

---
+++ ManageMe.Core.Api/Models/ExternalApplicants/ExternalApplicant.cs
@@ -0,0 +1,18 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+namespace ManageMe.Core.Api.Models.ExternalApplicants
+{
+    public class ExternalApplicant
+    {
+        public Guid ExternalApplicantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string GroupName { get; set; }
+    }
+}
---
+++ ManageMe.Core.Api/Models/Groups/Group.cs
@@ -0,0 +1,16 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Models.Applicants;
+
+namespace ManageMe.Core.Api.Models.Groups
+{
+    public class Group
+    {
+        public Guid GroupId { get; set; }
+        public string GroupName { get; set; }
+        public List<Applicant> Applicants { get; set; }
+    }
+}
