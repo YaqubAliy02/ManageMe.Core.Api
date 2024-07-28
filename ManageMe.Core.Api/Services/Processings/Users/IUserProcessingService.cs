9dcecf38df5d38617adf3b9bd2a7f5799bd03ddc
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-28 12:31:55 +0500

    PROCESSINGS: Add Users Service

---
+++ ManageMe.Core.Api/Services/Processings/Users/IUserProcessingService.cs
@@ -0,0 +1,19 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Models.Users;
+
+namespace ManageMe.Core.Api.Services.Processings.Users
+{
+    public interface IUserProcessingService
+    {
+        ValueTask<User> AddUserAsync(User user);
+        ValueTask<User> RetrieveUserByIdAsync(Guid userid);
+        ValueTask<User> RetrieveUserByEmailAndPasswordAsync(string email, string password);
+        IQueryable RetrieveAllUsers();
+        ValueTask<User> ModifyUserAsync(User user);
+        ValueTask<User> RemoveUserAsync(Guid userid);
+    }
+}
---
+++ ManageMe.Core.Api/Services/Processings/Users/UserProcessingService.cs
@@ -0,0 +1,38 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Models.Users;
+using ManageMe.Core.Api.Services.Foundations.Users;
+
+namespace ManageMe.Core.Api.Services.Processings.Users
+{
+    public class UserProcessingService : IUserProcessingService
+    {
+        private readonly IUserService userService;
+
+        public UserProcessingService(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async ValueTask<User> AddUserAsync(User user) =>
+           await this.userService.AddUserAsync(user);
+
+        public async ValueTask<User> RetrieveUserByIdAsync(Guid userid) =>
+            await this.userService.RetrieveUserByIdAsync(userid);
+
+        public async ValueTask<User> RetrieveUserByEmailAndPasswordAsync(string email, string password) =>
+            await this.userService.RetrieveUserByEmailAndPasswordAsync(email, password);
+
+        public IQueryable RetrieveAllUsers() =>
+            this.userService.RetrieveAllUsers();
+
+        public async ValueTask<User> ModifyUserAsync(User user) =>
+            await this.userService.ModifyUserAsync(user);
+
+        public async ValueTask<User> RemoveUserAsync(Guid userid) =>
+            await this.userService.RemoveUserAsync(userid);
+    }
+}
