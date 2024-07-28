a5679259dc097d40f507640b832cb798f8a5ae5e
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-07-28 12:15:52 +0500

    FOUNDATIONS: Add Spreadsheet Service

---
+++ ManageMe.Core.Api/Services/Foundations/Spreadsheets/ISpreadsheetService.cs
@@ -0,0 +1,14 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Models.ExternalApplicants;
+
+namespace ManageMe.Core.Api.Services.Foundations.Spreadsheets
+{
+    public interface ISpreadsheetService
+    {
+        List<ExternalApplicant> GetExternalApplicants(MemoryStream stream);
+    }
+}
---
+++ ManageMe.Core.Api/Services/Foundations/Spreadsheets/SpreadsheetService.cs
@@ -0,0 +1,28 @@
+﻿//===========================
+// Copyright (c) YaqubAliy Developer
+// Manage your academy easily
+//===========================
+
+using ManageMe.Core.Api.Brokers.Spreadsheets;
+using ManageMe.Core.Api.Models.ExternalApplicants;
+
+namespace ManageMe.Core.Api.Services.Foundations.Spreadsheets
+{
+    public class SpreadsheetService : ISpreadsheetService
+    {
+        private readonly ISpreadsheetBroker spreadsheetBroker;
+
+        public SpreadsheetService(ISpreadsheetBroker spreadsheetBroker)
+        {
+            this.spreadsheetBroker = spreadsheetBroker;
+        }
+
+        public List<ExternalApplicant> GetExternalApplicants(MemoryStream stream)
+        {
+            List<ExternalApplicant> externalApplicants =
+                            this.spreadsheetBroker.ImportApplicants(stream);
+
+            return externalApplicants;
+        }
+    }
+}
